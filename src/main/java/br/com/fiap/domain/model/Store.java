9a2de211a6688fe90f470f61c7d59151b1ec2593
João Vitor Almeida <jvitor.almeida98@gmail.com>
2022-07-10 17:40:50 -0300

    teste

--- src/main/java/br/com/fiap/domain/model/Store.java
+++ src/main/java/br/com/fiap/domain/model/Store.java
@@ -6,14 +6,10 @@ import io.quarkus.mongodb.panache.PanacheMongoEntity;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 
 
-@MongoEntity(collection = "store")
-public class Store extends PanacheMongoEntity {
+public class Store {
+    public String id;
     public String name;
     public String cnpj;
     public Long percent;
     public String urlLogo;
-
-    public static List<Store> findByName(String name) {
-        return find("name", name).list();
-    }
 }
